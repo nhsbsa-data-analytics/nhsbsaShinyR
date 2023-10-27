fab6847d2e2f568c6b5fc048ed68ac35b5c596cf
Mark McPherson <mark.mcpherson@nhsbsa.nhs.uk>
2023-10-27 11:19:08 +0100

    Add new module for additional markdown page

---
+++ R/mod_markdown_internal_link_example.R
@@ -0,0 +1,22 @@
+#' markdown_internal_link_example UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd
+mod_markdown_internal_link_example_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    includeMarkdown("inst/app/www/assets/markdown/mod_markdown_internal_link_example.md")
+  )
+}
+
+#' markdown_example Server Functions
+#'
+#' @noRd
+mod_markdown_internal_link_example_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    ns <- session$ns
+  })
+}
