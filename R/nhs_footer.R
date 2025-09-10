8872c77e5facadd20121e0ae2315562804644da3
Alistair Jones <alistair.jones@nhsbsa.nhs.uk>
2025-09-10 18:10:40 +0100

    Add crown copyright and align footer style with other bsa websites

--- R/nhs_footer.R
+++ R/nhs_footer.R
@@ -23,57 +23,65 @@ nhs_footer <- function(a11y_statement_url = "#",
         id = "nhsuk-footer",
         tags$div(
           class = "nhsuk-width-container app-width-container",
-          style = "display: flex; align-items: center;",
-          tags$ul(
-            class = "nhsuk-footer__list",
-            tags$li(
-              class = "nhsuk-footer__list-item",
-              a(
-                class = "nhsuk-footer__list-item-link",
-                style = "text-decoration: underline;",
-                href = a11y_statement_url,
-                target = "_blank",
-                "Accessibility statement"
+          style = "display: flex; flex-direction: column; align-items: flex-start; color: grey;",
+          div(
+            class = "nhsuk-footer__top-row",
+            style = "display: flex; justify-content: space-between; align-items: center; width: 100%;",
+            tags$ul(
+              class = "nhsuk-footer__list",
+              tags$li(
+                class = "nhsuk-footer__list-item",
+                a(
+                  class = "nhsuk-footer__list-item-link",
+                  style = "text-decoration: underline;",
+                  href = a11y_statement_url,
+                  target = "_blank",
+                  "Accessibility"
+                )
+              ),
+              tags$li(
+                class = "nhsuk-footer__list-item",
+                a(
+                  class = "nhsuk-footer__list-item-link",
+                  style = "text-decoration: underline;",
+                  href = paste0("mailto:@", email),
+                  target = "_blank",
+                  "Contact us"
+                )
+              ),
+              tags$li(
+                class = "nhsuk-footer__list-item",
+                a(
+                  class = "nhsuk-footer__list-item-link",
+                  style = "text-decoration: underline;",
+                  href = github_url,
+                  target = "_blank",
+                  "GitHub"
+                )
               )
             ),
-            tags$li(
-              class = "nhsuk-footer__list-item",
-              a(
-                class = "nhsuk-footer__list-item-link",
-                style = "text-decoration: underline;",
-                href = paste0("mailto:@", email),
-                target = "_blank",
-                "Contact us"
-              )
-            ),
-            tags$li(
-              class = "nhsuk-footer__list-item",
-              a(
-                class = "nhsuk-footer__list-item-link",
-                style = "text-decoration: underline;",
-                href = github_url,
-                target = "_blank",
-                "GitHub"
-              )
+            span(
+              style = "font-size: 1rem;",
+              HTML("&copy; Crown Copyright")
             )
           ),
           div(
             class = "nhsuk-footer__copyright",
-            style = "display: inline-flex; flex-direction: column; width: 30%;",
+            style = "margin-top: 1rem; display: flex; align-items: center; gap: 0.5rem; flex-wrap: nowrap; white-space: nowrap;",
             tags$image(
               class = "nhsuk-logo",
-              style = "height: 30px;",
+              style = "height: 17; width: 41;",
               src = "www/assets/logos/logo-ogl.svg",
               name = "Open Government License logo",
               alt = "Open Government License"
             ),
             span(
               "All content is available under the",
-              style = "text-align: center;",
               tags$a(
-                "Open Government Licence v3.0",
                 href = "https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/", # Exclude Linting
-                rel = "license"
+                rel = "license",
+                style = "color: grey; text-decoration: underline;",
+                "Open Government Licence v3.0,",
               ),
               "except where otherwise stated"
             )
