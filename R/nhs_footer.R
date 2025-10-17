32b60423f9b1e190d2620879783bff73c98efa87
MarkMc1089 <mark.mcpherson@nhsbsa.nhs.uk>
2025-10-17 13:04:37 +0100

    Feat: Updates footer to replicate layout and style of DDaT Playbook - Uses existing CSS classes from NHS Toolkit - Additional (and minimal) styling to match as closely as possible - Note we have always used a larger font size, which is kept

--- R/nhs_footer.R
+++ R/nhs_footer.R
@@ -21,19 +21,17 @@ nhs_footer <- function(a11y_statement_url = "#",
       tags$div(
         class = "nhsuk-footer",
         id = "nhsuk-footer",
+        h2(class = "nhsuk-u-visually-hidden", "Support links"),
         tags$div(
           class = "nhsuk-width-container app-width-container",
-          style = "display: flex; flex-direction: column; align-items: flex-start; color: grey;",
           div(
             class = "nhsuk-footer__top-row",
-            style = "display: flex; justify-content: space-between; align-items: center; width: 100%;",
             tags$ul(
               class = "nhsuk-footer__list",
               tags$li(
                 class = "nhsuk-footer__list-item",
                 a(
                   class = "nhsuk-footer__list-item-link",
-                  style = "text-decoration: underline;",
                   href = a11y_statement_url,
                   target = "_blank",
                   "Accessibility"
@@ -43,7 +41,6 @@ nhs_footer <- function(a11y_statement_url = "#",
                 class = "nhsuk-footer__list-item",
                 a(
                   class = "nhsuk-footer__list-item-link",
-                  style = "text-decoration: underline;",
                   href = paste0("mailto:@", email),
                   target = "_blank",
                   "Contact us"
@@ -53,7 +50,6 @@ nhs_footer <- function(a11y_statement_url = "#",
                 class = "nhsuk-footer__list-item",
                 a(
                   class = "nhsuk-footer__list-item-link",
-                  style = "text-decoration: underline;",
                   href = github_url,
                   target = "_blank",
                   "GitHub"
@@ -61,26 +57,25 @@ nhs_footer <- function(a11y_statement_url = "#",
               )
             ),
             span(
-              style = "font-size: 1rem;",
+              class = "nhsuk-footer__copyright",
               HTML("&copy; Crown Copyright")
             )
           ),
           div(
-            class = "nhsuk-footer__copyright",
-            style = "margin-top: 1rem; display: flex; align-items: center; gap: 0.5rem; flex-wrap: nowrap; white-space: nowrap;",  # Exclude linting
+            class = "nhsuk-ogl-footer",
             tags$image(
-              class = "nhsuk-logo",
-              style = "height: 17; width: 41;",
+              class = "nhsuk-ogl-logo",
               src = "www/assets/logos/logo-ogl.svg",
               name = "Open Government License logo",
               alt = "Open Government License"
             ),
             span(
+              class = "nhsuk-ogl-footer--text",
               "All content is available under the",
               tags$a(
                 href = "https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/", # Exclude Linting
                 rel = "license",
-                style = "color: grey; text-decoration: underline;",
+                target = "_blank",
                 "Open Government Licence v3.0,",
               ),
               "except where otherwise stated"
